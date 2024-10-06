661e719277b5dc83a3196188a59167e03e833fbb
Felipe Cerri <felipecamargocerri@gmail.com>
2024-10-06 20:45:46 -0300

    ta quase pronta a implementacao do bonus, so precisa corrigir alguns erros na logica

---
+++ Trabalho_06/heapsort_bonus.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct prato_ {
+    int prioridade, preparo;
+    char nome[51];
+    struct prato_ *esq, *dir;
+} prato ;
+    
+void heapSort(prato *cardapio, int tam);
+
+void buildHeap(prato *cardapio, int tam);
+
+void heapify(prato *atual);
+
+void buildTree(prato *cardapio, int tam);
+
+int compare(prato a, prato b); //retorna 1 se a > b
+                               //retorna 0 se a < b
+
+                               
+void swap(prato *pai, prato *filho);
+
+double endTime(clock_t start){
+    clock_t finish = clock();
+    return ((double)(finish - start))/CLOCKS_PER_SEC;
+}
+
+int main () {
+    int n; scanf("%d", &n);
+    prato *cardapio = malloc(sizeof(prato)*n);
+    for (int i =0; i<n; i++) {
+        scanf("%d%d %s", &(cardapio[i]).prioridade, &(cardapio[i]).preparo, cardapio[i].nome);
+    }
+
+    double tempo; clock_t start = clock();
+    heapSort(cardapio, n);
+    tempo = endTime(start);
+
+    for (int i = 0; i<n; i++) printf("%s\n", cardapio[i].nome);
+    // printf("\ntempo de execução: %lf", tempo);
+
+    free(cardapio);
+    return 0;
+}
+
+void heapify(prato *atual) {
+    prato *maior = atual;
+    int trocas = 0;
+
+    if (atual->esq != NULL && compare(*(atual->esq), *atual)) { //acha maior filho
+        maior = atual->esq;
+        trocas++;
+    }   
+    if (atual->dir != NULL && compare(*(atual->dir), *maior) ) {
+        maior = atual->dir;
+        trocas++;
+    } 
+    if (trocas == 0) return; //caso o pai seja maior que os filhos
+    
+    swap(maior, atual); //troca o pai com o maior filho
+    heapify(maior); //chama o arranjo para o filho que estava desordenado
+
+}
+
+void buildHeap(prato *cardapio, int tam) {
+    for (int i = ((tam)/2 - 1) ; i>=0; i--) { //rearrajo a partir do primeiro que nao e folha
+        heapify(&cardapio[i]); 
+    }
+}
+
+void heapSort(prato *cardapio, int tam) {
+    buildTree(cardapio, tam);
+    buildHeap(cardapio, tam);
+    
+    for (int i = tam - 1; i > 0; i--) {
+        swap(&cardapio[0], &cardapio[i]); //maior vai para o final do array
+
+        //isola o maior elemento da arvore
+        if ((i/2)%2) { //filho é da forma 2k + 1 (impar), portanto é o filho da esquerda
+            cardapio[i/2].esq = NULL;
+        }
+        else cardapio[i/2].dir = NULL;
+
+        heapify(&cardapio[0]);//estabelece max heap com raiz em idx 0
+    }
+}
+
+int compare(prato a, prato b) {        //1 se a > b
+    if (a.prioridade == b.prioridade) {//0 se a < b
+        if (a.preparo < b.preparo)
+            return 1;
+        else 
+            return 0;
+    }
+
+    if (a.prioridade > b.prioridade)
+        return 1;
+    else
+        return 0;
+}
+
+void buildTree(prato *cardapio, int tam) {
+    for (int i =0; i<(tam/2); i++) { //constroi a arvore
+        cardapio[i].esq = &cardapio[2*i + 1];
+        cardapio[i].dir = &cardapio[2*i + 2];
+    }
+
+    for (int i = (tam/2) -1; i<tam; i++) { //folhas apontam para nulo 
+        cardapio[i].esq = NULL;
+        cardapio[i].dir = NULL;
+    }
+}
+
+void swap(prato *pai, prato *filho) { //o problema nao ta no swap, ambas as logicas funcionam   
+    int temp = pai->preparo;
+    pai->preparo = filho->preparo;
+    filho->preparo = temp;
+
+    temp = pai->prioridade;
+    pai->prioridade = filho->prioridade;
+    filho->prioridade = temp;
+
+    char tempString[51];
+    strcpy(tempString, pai->nome);
+    strcpy(pai->nome, filho->nome);
+    strcpy(filho->nome, tempString);
+
+
+    // prato temp = *pai; //troca do pai com o filho
+    // *pai = *filho;
+    // *filho = temp;
+
+    // prato *filho_esq = pai->esq, //troca dos filhos
+    //       *filho_dir = pai->dir;
+
+    // pai->esq = filho->esq;
+    // pai->dir = filho->dir;
+
+    // filho->esq = filho_esq;
+    // filho->dir = filho_dir;
+}
