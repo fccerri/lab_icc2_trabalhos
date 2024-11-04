ebe78253ef7b2aaef9660f7c7bdd081182f82313
Felipe Cerri <felipecamargocerri@gmail.com>
2024-11-04 19:48:16 -0300

    codigo finalizado

--- Trabalho_09/ABB.c
+++ Trabalho_09/ABB.c
@@ -1,83 +1,94 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
+// double endTime(clock_t start){
+//     clock_t finish = clock();
+//     return ((double)(finish - start))/CLOCKS_PER_SEC;
+// }
 
 typedef struct no_ {
-    struct no_ *filho_esq, *filho_dir;
-    int dado;
+    struct no_ *filho_esq; //ponteiro para o filho esquerdo
+    struct no_ *filho_dir; ////ponteiro para o filho direito
+    int dado; //dado contido no nó
 } NO;
 
 NO *constroiABB(int n);
-
-void insereNo(int valor, NO *raiz);
-
+NO *insereNo(int valor, NO *raiz);
 void BuscaCaminho(NO *raiz, int valor);
-
-void liberaArvore(NO *raiz);
+void liberaArvore(NO **raiz);
 
 int main () {
     int n, aux; scanf("%d", &n);
 
     NO *raiz = constroiABB(n);
 
+    // double tempo; clock_t start = clock();
     scanf("%d", &aux);
     BuscaCaminho(raiz, aux);
+    printf("\n");
+    // tempo = endTime(start);
 
-    //liberaArvore(raiz);
+    // printf("tempo de busca %lf", tempo);
+    liberaArvore(&raiz);
     return 0;
 }
 
 NO *constroiABB(int n) {
-    int temp; NO *raiz;
-    raiz = calloc(1, sizeof(NO));
+    int temp; NO *raiz; //ponteiro para a raiz da arvore
 
-    for (int i = 0; i<n; i++) {
+    scanf("%d", &temp);
+    raiz = insereNo(temp, raiz); //estabelece  e aloca a raiz da arvore
+
+    for (int i = 0; i<n -1 ; i++) { //constroi a arvore 
         scanf("%d", &temp);
-        insereNo(temp, raiz);
+        insereNo(temp, raiz); //insere o nó com o valor "temp"
     }
 
     return raiz;
 }
 
-void insereNo(int valor, NO *raiz){
+NO *insereNo(int valor, NO *raiz){
+    if (raiz == NULL) { //caso base, insere o novo nó
+        NO *aux = calloc(1, sizeof(NO)); //aloca o novo nó
+        aux->dado = valor; //guarda o seu valor
+        return aux;
+    }
     
     if (valor >= raiz->dado) { //caso valor seja MAIOR que a raiz (insercao no lado DIREITO)
-
-        if (raiz->filho_dir != NULL) { //caso o filho direito da raiz já exista
-            insereNo(valor, raiz->filho_dir); //faz chamada recursiva para o filho direito
-        }
-        else { //caso o filho direito não exista
-            raiz->filho_dir = calloc(1, sizeof(NO)); //aloca espaço para o nó folha
-            raiz->filho_dir->dado = valor; //insere valor 
-        }
+        raiz->filho_dir =  insereNo(valor, raiz->filho_dir); //faz chamada recursiva para o filho direito
     }
-
-    else if (valor < raiz->dado) { //caso valor seja MENOR que a raiz (insercao no lado ESQUERDO)
-
-        if (raiz->filho_esq != NULL) { //caso o filho esquerdo da raiz já exista
-            insereNo(valor, raiz->filho_esq); //faz chamada recursiva para o filho esquerdo
+    else { //caso valor seja MENOR que a raiz (insercao no lado ESQUERDO)
+            raiz->filho_esq = insereNo(valor, raiz->filho_esq); //faz chamada recursiva para o filho esquerdo
         }
-        else { //caso o filho esquerdo não exista
-            raiz->filho_esq = calloc(1, sizeof(NO)); //aloca espaço para o nó folha
-            raiz->filho_esq->dado = valor; //insere valor 
-        }
-    }
+    
+    return raiz;
 }
 
 void BuscaCaminho(NO *raiz, int valor) {
     if (raiz == NULL) { //caso o valor não esteja contido na arvore
-        printf("-1\n");
+        printf("-1");
         return;
     }
 
-    printf("%d ", raiz->dado); //printa no atual
+    printf("%d ", raiz->dado); //printa o nó atual
 
-    if (raiz->dado > valor) {
+    if (raiz->dado > valor) { //continua busca na arvore da esquerda
         BuscaCaminho(raiz->filho_esq, valor);
     }
-    else if (raiz->dado < valor) {
+    else if (raiz->dado < valor) { //continua busca na arvore da direita
         BuscaCaminho(raiz->filho_dir, valor);
-    }
+    }   
+}
 
-    if (raiz->dado  == valor) return;
-    
+void liberaArvore(NO **raiz) {
+    if (*raiz != NULL) {
+        liberaArvore(&(*raiz)->filho_dir); //chamada recursiva para arvore da direita
+        liberaArvore(&(*raiz)->filho_esq); //chamada recursiva para arvore da esquerda
+        (*raiz)->filho_esq = NULL;
+        (*raiz)->filho_dir = NULL;
+
+        free(*raiz); //libera a memoria
+        *raiz = NULL;
+    }
 }
